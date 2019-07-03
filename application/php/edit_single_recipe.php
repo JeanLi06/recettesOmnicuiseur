256489ad14cb34e3fb51da8d0c992cbdf20a5bec
Jean Liautaud <jliautaud@free.fr>
2019-07-03 18:43:31 +0200

    remplissage des champs pour l'édition d'une recette

---
+++ application/php/edit_single_recipe.php
@@ -0,0 +1,7 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: jean-utilisateur
+     * Date: 28/06/2019
+     * Time: 15:07
+     */
---
+++ js/main.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// ecouteur d'évènement sur le changement du fichier photo de edit_single_recipe.phtml
+$(function () {
+    $('#photo').change( function () {
+        // on change le src de l'image
+        $('#image_recette').attr('src', 'img/test.jpg');
+    })
+});
+
+/////////////Update de l'image après choix utilisateur sur pages update & create////////////////
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#image_recette').attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$("#photo").change(function(){
+    readURL(this);
+});
+////////////////////////////////////////////////////////////////////////////////////////////////
