643927ad789acca716b29cdd69702fb646fe9761
Jean Liautaud <jliautaud@free.fr>
2019-10-14 19:27:26 +0200

    refactoring flash_message.php

--- application/php/flash_message.php
+++ application/php/flash_message.php
@@ -3,26 +3,21 @@
 // Gestion des messages flash
     
     if ((!empty($_SESSION['flash_error_message']) && !empty($_SESSION['flash_error_message'])) || (isset($_SESSION['flash_confirm_message']) && !empty($_SESSION['flash_confirm_message']))) {
-//      Charge la bonne classe CSS dans le html, selon le type de message
-        echo '<div class="';
+//      On écrit la bonne classe CSS dans le html, selon le type de message
         if (!empty($_SESSION['flash_error_message']))
-            echo 'flash-error';
+            echo '<div class="flash-error" id="flash_message">';
         elseif (!empty($_SESSION['flash_confirm_message']))
-            echo 'flash-confirm';
-        echo '" id="flash_message">';
-//        On identifie le type de message : erreur ou confirmation
+            echo '<div class="flash-confirm" id="flash_message">';
+//      On identifie le type de message : erreur ou confirmation
         if (!empty($_SESSION['flash_error_message']))
             echo $_SESSION['flash_error_message'];
         elseif (!empty($_SESSION['flash_confirm_message']))
             echo $_SESSION['flash_confirm_message'];
-//        Un petit effet d'affichage avec jQuery...
-        echo '</div>
-        <script type="text/javascript">
-            $(function () {
-//                $("#flash_message").slideDown("slow").delay(2000).slideUp("low");
-                $("#flash_message").delay(2000).slideUp("low");
-            });
-        </script>';
-        unset($_SESSION['flash_error_message']);
-        unset($_SESSION['flash_confirm_message']);
+        echo '</div>';
+        // Insertion de jQuery : Cache un message flash (progressivement) dans une page, identifié par son id
+        echo '<script>$(function () {
+        $("#flash_message").delay(2000).slideUp("low");
+    });</script>';
+        unset($_SESSION['flash_error_message'], $_SESSION['flash_confirm_message']);
     }
+    
